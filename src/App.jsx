7395ca9a5f08cc3869ffc9add381024aa28bcff5
Oleksandr FIlippov <filya170895@gmail.com>
2023-09-09 16:37:33 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { bigShoe1 } from "./assets/images";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <img src={bigShoe1} alt="shoe" />
+    </>
+  );
+}
